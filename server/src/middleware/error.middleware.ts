29c1e3c1ed438450c710b2782a6e7684da19310e
Emanual Joyal <emanualjoyalka@gmail.com>
2026-06-28 18:11:59 +0530

    complete:setting up

---
+++ server/src/middleware/error.middleware.ts
@@ -0,0 +1,15 @@
+import type { NextFunction, Request, Response } from "express";
+import ApiError from "../utils/ApiError.js";
+
+
+const errorHandler = (err:Error|ApiError, req:Request, res:Response, next:NextFunction) => {
+    const statusCode = err instanceof ApiError ? err.statusCode : 500
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message
+    });
+
+};
+
+export default errorHandler;
